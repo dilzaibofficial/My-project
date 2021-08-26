7952d0015585a7d71851ef1f544b0b092031a7cb
DIL ZAIB <dilzaib5848@gmail.com>
2021-08-26 22:41:17 +0500

    third

---
+++ index.js
@@ -0,0 +1,42 @@
+// // javascript variable
+// // what are vairables? - container that store values
+
+
+// // in javascript there are 3 datatypes
+
+// // 1.numbers:
+// var num1 = 445;
+// var num2 = 33;
+
+// // 2.string:
+// var str1 ="dilzaib";
+// var str2 = 'shahzaib';
+
+// //3.object:
+// var mark={
+//     shah:33,
+//     dil:344,
+//     palh:55,
+//     num3:'palhg5848'
+// }
+// var a = 33;
+// var b =66;
+
+// var c = b;
+
+
+
+// console.log(a==b);
+// console.log(a!=b);
+// console.log("here is a+b is equal to",a+b);
+
+
+function avg(a,b){
+    return(a+b)/2;
+}
+
+var c1=avg(4,5);
+
+var c2=avg(55,65);
+
+console.log(c1,c2)
